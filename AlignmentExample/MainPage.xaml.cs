ec31d5e39255cd4a4f418915de1fe563722c9cae
pegaser <sperezgabriel@gmail.com>
2023-02-05 17:24:30 -0600

    Agregar archivos de proyecto.

---
+++ AlignmentExample/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿namespace AlignmentExample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnHorizontalStartClicked(object sender, EventArgs e)
+        {
+            target.HorizontalOptions = LayoutOptions.Start;
+        }
+        private void OnHorizontalCenterClicked(object sender, EventArgs e)
+        {
+            target.HorizontalOptions = LayoutOptions.Center;
+        }
+
+        private void OnHorizontalEndClicked(object sender, EventArgs e)
+        {
+            target.HorizontalOptions = LayoutOptions.End;
+        }
+        private void OnHorizontalFillClicked(object sender, EventArgs e)
+        {
+            target.HorizontalOptions = LayoutOptions.Fill;
+        }
+
+        private void OnVerticalStartClicked(object sender, EventArgs e)
+        {
+            target.VerticalOptions = LayoutOptions.Start;
+        }
+        private void OnVerticalCenterClicked(object sender, EventArgs e)
+        {
+            target.VerticalOptions = LayoutOptions.Center;
+        }
+
+        private void OnVerticalEndClicked(object sender, EventArgs e)
+        {
+            target.VerticalOptions = LayoutOptions.End;
+        }
+        private void OnVerticalFillClicked(object sender, EventArgs e)
+        {
+            target.VerticalOptions = LayoutOptions.Fill;
+        }
+    }
+}
---
+++ Maui.Layouts/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace Maui.Layouts;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+        this.btn.Clicked += Btn_Clicked;
+	}
+
+    private void Btn_Clicked(object sender, EventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+}
+
